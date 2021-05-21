743c8d19020033f380128810089b846e6589c9e1
edwaangu <edwaangu171@outlook.com>
2021-05-21 12:59:18 -0400

    I have gone ahead and completed it

--- ForLoops/Form1.cs
+++ ForLoops/Form1.cs
@@ -19,28 +19,51 @@ namespace ForLoops
 
         private void runButton_Click(object sender, EventArgs e)
         {
+            q1Output.Text = "";
+            q2Output.Text = "";
+            q3Output.Text = "";
+            q4Output.Text = "";
+            q5Output.Text = "";
+            q6Output.Text = "";
+
             ///1.  Create a loop that displays all values inclusive between 1-5.
 
             for (int i = 1; i <= 5; i++)
             {
-                
+                q1Output.Text += $"{i} ";
             }
 
 
             ///2.  Create a loop that displays all values inclusive between 10-6.
-
+            for (int i = 10; i >= 6; i--)
+            {
+                q2Output.Text += $"{i} ";
+            }
 
             ///3.  Create a loop that displays even numbers inclusive between 2 and 10
-
+            for (int i = 2; i <=10; i += 2)
+            {
+                q3Output.Text += $"{i} ";
+            }
 
             ///4.  Create a loop that displays the sum of all numbers inclusive 
             ///between 1 and 10. The answers should be 55.
-
+            int value = 0;
+            for(int i = 1; i <= 10; i++)
+            {
+                value += i;
+            }
+            q4Output.Text = $"{value}";
 
             ///5. Create a loop that displays all leap years inclusive between 
             ///1990 and 2010. The output should be: 1992 1998 2000 2004 2008
-
-
+            for(int year = 1990; year <= 2010; year++)
+            {
+                if(year%4 == 0)
+                {
+                    q5Output.Text += $"{year} ";
+                }
+            }
 
             ///6. Print the following patterns using a loop:
             ///
@@ -52,6 +75,13 @@ namespace ForLoops
             /// 
             /// Hint: Start off with a string that has 1 "*" in it, then 
             /// in the loop you will add another "*" using +=
+
+            string theOutput = "*";
+            for (int i = 0;i < 5;i++)
+            {
+                q6Output.Text += theOutput + "\n";
+                theOutput += "*";
+            }
         }
     }
 }
